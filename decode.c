caa1ab61033eef0ecfc05e1b28035ac4a42e7e96
Liwei Wang <lw.wang@gmail.com>
2017-08-06 23:47:27 +0800

    commit

--- decode.c
+++ decode.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -64,6 +65,228 @@ int decode(PDATA data, int length) {
 }
 
 int decode_062(PSDPS data, int length) {
+   if(data->cat != 62) {
+     printf("Not CAT 062 DATA. Ignore. CAT = %#x\n", data->cat);
+     return -1;
+   }
+   printf("Data CAT 062. ");
+   int data_len = ntohs(*(short *)(data->length));
+   printf("data length = %d\n", data_len);
+   if(data_len != length) {
+     printf("Data length is incorrent. Ignore.");
+     return -2;
+   }
 
+   printf("\nDecoding data.....\n\n");
+   char* p = data->data;
+  // while(p && p < data->data + data_len) {
+      p = decode_062_fspec(p);
+  // }
 
+   return 0;
+}
+
+char* decode_062_fspec(char* data){
+    /* only support FRN is not bigger than 28. */
+    unsigned int FRN = 0;
+    int count = 0;
+    do {
+      count++;
+      for(int i = 7; i > 0; i--) {
+        FRN |= (*data >> i & 1) << (7 * count - i);
+      }
+      if(count > 5) {
+        printf("Fatal Error: FRN is incorrect. Count is %d.\n", count);
+        return NULL;
+      }
+    }while(*(data++) & 1);
+
+    for(int i = 0; i < 28; i++) {
+      if((FRN >> i) & 1) {
+        decode_items func = funcs[i];
+        if(func) data = func(data);
+      }
+    }
+
+    return data;
+}
+
+char* decode_062_010(char* data){
+    printf("I062/010 :\n");
+    printf("\tSAC = %u, ", *data);
+    data++;
+    printf("SIC = %u\n", *data);
+    data++;
+    return data;
+}
+
+char* decode_062_015(char* data){
+    return data;
+}
+
+char* decode_062_070(char* data){
+    unsigned int time = (data[0] << 16 & 0xff0000) | (data[1] << 8 & 0xff00) | (data[2] & 0xff);
+    printf("I062/070 :\n");
+    printf("\tTime of track information: %u, about %f seconds, %f hours.\n", time, time/128.0, time/128.0/3600.0);
+    data += 3;
+    return data;
+}
+
+char* decode_062_105(char* data){
+    int latitude = ntohl(*(int*)data);
+    data+=4;
+    int longtitude = ntohl(*(int*)data);
+    data+=4;
+    printf("I062/105 :\n");
+    printf("\tLatitude : (orignal) %d  ===> %f degrees\n\tLongtitude : (orignal) %d ===> %f degrees\n",
+              latitude, latitude * 180.0 / pow(2, 25),
+              longtitude, longtitude * 180.0 / pow(2, 25));
+    return data;
+}
+
+char* decode_062_100(char* data){
+  int X = (data[0] << 16 & 0xff0000) | (data[1] << 8 & 0xff00) | (data[2] & 0xff);
+  data+=3;
+  int Y = (data[0] << 16 & 0xff0000) | (data[1] << 8 & 0xff00) | (data[2] & 0xff);
+  data+=3;
+  printf("I062/110 :\n");
+  printf("\tX : (orignal) %d ===> %f m\n\tY : (orignal) %d ===> %f m\n",
+            X, X * 0.5, Y, Y * 0.5);
+    return data;
+}
+
+char* decode_062_185(char* data){
+    short vx = ntohs(*(short *)(data));
+    data+=2;
+    short vy = ntohs(*(short *)(data));
+    data+=2;
+    printf("I062/185 :\n");
+    printf("\tvx = %f m/s, vy = %f m/s, V(abs) = %f km/h\n", vx * 0.25, vy * 0.25, sqrt((vx * vx + vy * vy) * 0.0625) * 3600 / 1000.0);
+    return data;
+}
+
+char* decode_062_210(char* data){
+    return data;
+}
+
+char* decode_062_060(char* data){
+    // TODO::
+    printf("I062/060 :\n");
+    printf("\tHex data is : 0x%02X%02X\n", (unsigned char)data[0], (unsigned char)data[1]);
+    data += 2;
+    return data;
+}
+
+char* decode_062_245(char* data){
+    // TODO::
+    printf("I062/245 :\n");
+    printf("\tHex data is : 0x%02X%02X%02X%02X%02X%02X%02X\n",
+            (unsigned char)data[0], (unsigned char)data[1],
+            (unsigned char)data[2], (unsigned char)data[3],
+            (unsigned char)data[4], (unsigned char)data[5],
+            (unsigned char)data[6]);
+    data += 7;
+    return data;
+}
+
+char* decode_062_380(char* data){
+  // TODO::
+  printf("FIX ME:: I062/380\n");
+  return NULL;
+}
+
+char* decode_062_040(char* data){
+    // TODO::
+    printf("I062/040 :\n");
+    printf("\tHex data is : 0x%02X%02X\n", (unsigned char)data[0], (unsigned char)data[1]);
+    data += 2;
+    return data;
+}
+
+char* decode_062_080(char* data){
+    // TODO::
+    printf("I062/80 :\n");
+    int count = 1;
+    do {
+      printf("\tOctet %d: 0x%02X\n", count, (unsigned char)*data);
+      if(count > 4) {
+        printf("Decode I062/80 failed. Count exceed 4.");
+        return NULL;
+      }
+      count++;
+    }while(*data++ & 1);
+    return data;
+}
+
+char* decode_062_290(char* data){
+    return data;
+}
+
+char* decode_062_200(char* data){
+    return data;
+}
+
+char* decode_062_295(char* data){
+    return data;
+}
+
+char* decode_062_136(char* data){
+    short level = ntohs(*(short*)data);
+    printf("I062/136 :\n");
+    printf("\tMeasured flight level: (orignal)%d ===> %f FL\n", level, level * 0.25);
+    data += 2;
+    return data;
+}
+
+char* decode_062_130(char* data){
+    return data;
+}
+
+char* decode_062_135(char* data){
+    // TODO:: minus value should be recalculated. Need more information.
+    short level = ntohs(*(short*)data);
+    unsigned int QNH = level & 0x8000;
+    level = (level & 0x3fff) | ((level & 0x4000) << 1);
+    printf("I062/136 :\n");
+    printf("\tQNH correction applied: %u\n", QNH);
+    printf("\tBarometric altitude: (orignal)%d ===> %f FL\n", level, level * 0.25);
+    data += 2;
+    return data;
+}
+
+char* decode_062_220(char* data){
+    return data;
+}
+
+char* decode_062_390(char* data){
+
+    return data;
+}
+
+char* decode_062_270(char* data){
+    return data;
+}
+
+char* decode_062_300(char* data){
+    return data;
+}
+
+char* decode_062_110(char* data){
+    return data;
+}
+
+char* decode_062_120(char* data){
+    return data;
+}
+
+char* decode_062_510(char* data){
+    return data;
+}
+
+char* decode_062_500(char* data){
+    return data;
+}
+
+char* decode_062_340(char* data){
+    return data;
 }
--- decode.h
+++ decode.h
@@ -11,24 +11,74 @@ typedef struct __attribute__((packed)) _data {
 }DATA, *PDATA;
 
 typedef struct __attribute__((packed)) _sdps {
-
-
-
+    char cat;
+    char length[2];
+    char data[0];
 }SDPS, *PSDPS;
-
-enum {
-   I062_010 = 1,
-   SPARE1,
-   I062_015,
-   I062_070,
-   I062_105,
-   I062_100,
-   I062_185,
-}FRN;
-
-
 
 int decode(PDATA data, int length);
 int decode_062(PSDPS data, int length);
+
+char* decode_062_fspec(char* data);
+char* decode_062_010(char* data);
+char* decode_062_015(char* data);
+char* decode_062_070(char* data);
+char* decode_062_105(char* data);
+char* decode_062_100(char* data);
+char* decode_062_185(char* data);
+char* decode_062_210(char* data);
+char* decode_062_060(char* data);
+char* decode_062_245(char* data);
+char* decode_062_380(char* data);
+char* decode_062_040(char* data);
+char* decode_062_080(char* data);
+char* decode_062_290(char* data);
+char* decode_062_200(char* data);
+char* decode_062_295(char* data);
+char* decode_062_136(char* data);
+char* decode_062_130(char* data);
+char* decode_062_135(char* data);
+char* decode_062_220(char* data);
+char* decode_062_390(char* data);
+char* decode_062_270(char* data);
+char* decode_062_300(char* data);
+char* decode_062_110(char* data);
+char* decode_062_120(char* data);
+char* decode_062_510(char* data);
+char* decode_062_500(char* data);
+char* decode_062_340(char* data);
+
+typedef char* (*decode_items)(char *);
+
+decode_items funcs[28] = {
+  decode_062_010,
+  NULL,
+  decode_062_015,
+  decode_062_070,
+  decode_062_105,
+  decode_062_100,
+  decode_062_185,
+  decode_062_210,
+  decode_062_060,
+  decode_062_245,
+  decode_062_380,
+  decode_062_040,
+  decode_062_080,
+  decode_062_290,
+  decode_062_200,
+  decode_062_295,
+  decode_062_136,
+  decode_062_130,
+  decode_062_135,
+  decode_062_220,
+  decode_062_390,
+  decode_062_270,
+  decode_062_300,
+  decode_062_110,
+  decode_062_120,
+  decode_062_510,
+  decode_062_500,
+  decode_062_340,
+};
 
 #endif
